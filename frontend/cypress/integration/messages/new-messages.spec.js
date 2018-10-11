8601c861582edf571de55d599315bdd9d0198841
Scott Warren <realscottwarren@gmail.com>
2018-10-11 16:19:06 +0300

    Adding spec for generate new message button

--- frontend/cypress/integration/messages/new-messages.spec.js
+++ frontend/cypress/integration/messages/new-messages.spec.js
@@ -3,7 +3,19 @@ context('Actions', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('should not show new messages by default', () => {
-    cy.get('.Header-container').not('contain', '.UnreadStatus-badge')
+  describe('New messages should be represented in the header', () => {
+    it('should not show new messages by default', () => {
+      cy.get('.Header-container')
+        .not('contain', '.UnreadStatus-badge')
+        .not('contain', 'New')
+    })
+
+    it('should generate a new unread message', () => {
+      // Get existing count of messages?
+
+      cy.get('.NewMessageGenerator-button').click()
+
+      cy.get('.UnreadStatus-badge').should('contain', 'New')
+    })
   })
 })
