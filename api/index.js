5f32ceac8207465cb0915eb01b0cffde7472aca4
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-07-30 12:04:25 +0300

    Fixing reference to wrong database

--- api/index.js
+++ api/index.js
@@ -11,34 +11,36 @@ const app = express()
 
 // Enable CORS so the frontend can access the API on a different port/host name without issues
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "POST,PUT,GET,PATCH")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'POST,PUT,GET,PATCH')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
   next()
 })
 
 // Configure app to parse our responses as JSON (since we're an API)
 app.use(logger('dev'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended:true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
-
 
 // Port for the local development server
 const port = process.env.PORT || 3001
 
 // Connect to local Mongo database
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/getselected-scott', {
+mongoose.connect('mongodb://localhost/code-showcase', {
   useNewUrlParser: true,
-});
+})
 
 // Enable sourcemap's so we see the information from the source of the problem, rather than the compiled output
 SourceMapSupport.install()
 
 app.use('/messages', messageRoutes)
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   return res.end('Work work!')
 })
 
